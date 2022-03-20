83b0f02f5606c2ed1aae163a6c245dafb4b4b4f3
maru44 <takumaru22244@gmail.com>
2022-03-20 16:52:00 +0900

    change dir

--- _sample/gen.go
+++ _sample/gen.go
@@ -1,3 +1,3 @@
 package sample
 
-//go:generate go run github.com/maru44/simdb/gen simdb.yaml main generated.go sample
+//go:generate go run github.com/maru44/simdb simdb.yaml main generated.go sample
