571c63f172e3f7e4c11ea0980b2d3066b413a5f1
maru44 <takumaru22244@gmail.com>
2022-03-20 13:11:19 +0900

    add mutex

--- _sample/sample/generated.go
+++ _sample/sample/generated.go
@@ -2,7 +2,10 @@
 
 package sample
 
-import "pkg/errors"
+import (
+	"pkg/errors"
+	"sync"
+)
 
 type (
 	TableSample struct {
@@ -11,26 +14,80 @@ type (
 		Column3 uint64
 	}
 
-	TableSamples map[int]TableSample
+	TableSamples struct {
+		Data map[int]TableSample
+		mux  sync.Mutex
+	}
 )
 
-func InitTableSample() TableSamples {
-	return map[int]TableSample{}
+func NewTableSample() TableSamples {
+	return TableSamples{
+		Data: map[int]TableSample{},
+	}
 }
 
-func (t *TableSamples) Get(id int) (TableSample, error) {
-	v, ok := t[id]
+func (t TableSamples) Get(id int) (TableSample, error) {
+	v, ok := t.Data[id]
 	if !ok {
 		return TableSample{}, errors.New("Not Exists")
 	}
 	return v, nil
 }
 
-func (t *TableSamples) Create(id int, value TableSample) error {
-	v, ok := t[id]
-	if ok {
+func (t TableSamples) Insert(id int, value TableSample) error {
+	if _, ok := t.Data[id]; ok {
 		return errors.New("Already Exists")
 	}
-	v[id] = value
+	t.mux.Lock()
+	defer t.mux.Unlock()
+	t.Data[id] = value
 	return nil
+}
+
+func (t TableSamples) BulkInsert(values TableSamples) error {
+	t.mux.Lock()
+	defer t.mux.Unlock()
+	for id, value := range values {
+		if _, ok := t.Data[id]; ok {
+			return errors.New("Already Exists")
+		}
+		t.Data[id] = value
+	}
+	return nil
+}
+
+func (t TableSamples) Update(id int, value TableSample) error {
+	if _, ok := t.Data[id]; !ok {
+		return errors.New("Not Exists")
+	}
+	t.mux.Lock()
+	defer t.mux.Unlock()
+	t.Data[id] = value
+	return nil
+}
+
+func (t TableSamples) Upsert(id int, value TableSample) {
+	t.mux.Lock()
+	defer t.mux.Unlock()
+	t.Data[id] = value
+}
+
+func (t TableSamples) BulkUpsert(values TableSamples) {
+	t.mux.Lock()
+	defer t.mux.Unlock()
+	for id, value := range values {
+		t.Data[id] = value
+	}
+}
+
+func (t TableSamples) Delete(id int) {
+	t.mux.Lock()
+	defer t.mux.Unlock()
+	delete(t, id)
+}
+
+func (t TableSamples) Truncate() {
+	t.mux.Lock()
+	defer t.mux.Unlock()
+	t.Data = map[int]TableSample{}
 }
